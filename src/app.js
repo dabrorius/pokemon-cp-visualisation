dc833a14df1ce12a18fa0bb05bb71322e7e10f36
Filip Defar <filip.defar@toptal.com>
2017-03-09 13:12:44 +0100

    Extract details view to own component

--- src/app.js
+++ src/app.js
@@ -25,13 +25,6 @@ var searchMatchOpacityScale = d3.scaleOrdinal()
   .domain([true, false])
   .range([1, 0.2]);
 
-var barLength = 100;
-var cpBarScale = d3.scaleLinear().domain([0, 3670]).range([0, barLength]);
-var hpBarScale = d3.scaleLinear().domain([0, 415]).range([0, barLength]);
-var attackScale = d3.scaleLinear().domain([0, 330]).range([0, barLength]);
-var defenseScale = d3.scaleLinear().domain([0, 396]).range([0, barLength]);
-var staminaScale = d3.scaleLinear().domain([0, 510]).range([0, barLength]);
-
 d3.csv("pokemon.csv", function(error, rawData) {
   if (error) throw error;
 
@@ -45,58 +38,6 @@ d3.csv("pokemon.csv", function(error, rawData) {
 
   var detailsView = new DetailsView(svg);
 
-  var detailsScreen = svg.append('g');
-  var pokemonName = detailsScreen.append('text')
-    .attr('x', 50)
-    .attr('y', 50)
-    .attr('font-family', 'verdana')
-    .attr('font-size', '16px');
-
-  var barHeight =  8;
-  var barXPos = 50;
-  var barYPos = 80;
-  var barPadding = 20;
-  var labelPadding = 2;
-
-  var labelXPos = 50;
-
-  var createBar = function(x, y, fill) {
-    detailsScreen.append('rect')
-      .attr('x', x)
-      .attr('y', y)
-      .attr('height', barHeight)
-      .attr('fill', '#eee')
-      .attr('width', barLength);
-
-    return detailsScreen.append('rect')
-      .attr('x', x)
-      .attr('y', y)
-      .attr('height', barHeight)
-      .attr('fill', fill)
-      .attr('width', 0);
-  }
-
-
-  var cpBar = createBar(barXPos, barYPos, '#35ADC5');
-  var hpBar = createBar(barXPos, barYPos + (barHeight + barPadding), '#B3D5D6');
-  var attackBar = createBar(barXPos, barYPos + (barHeight + barPadding) * 2, '#B69689');
-  var defenseBar = createBar(barXPos, barYPos + (barHeight + barPadding) * 3, '#D7B19C');
-  var staminaBar = createBar(barXPos, barYPos + (barHeight + barPadding) * 4, '#E5D1B9');
-
-  var createLabel = function(x, y) {
-    return detailsScreen.append('text')
-      .attr('x', x)
-      .attr('y', y)
-      .attr('font-family', 'verdana')
-      .attr('font-size', '10px');
-  }
-
-  var cpLabel = createLabel(labelXPos, barYPos - labelPadding);
-  var hpLabel = createLabel(labelXPos, barYPos - labelPadding + (barHeight + barPadding));
-  var attackLabel = createLabel(labelXPos, barYPos - labelPadding + (barHeight + barPadding) * 2);
-  var defenseLabel = createLabel(labelXPos, barYPos - labelPadding + (barHeight + barPadding) * 3);
-  var staminaLabel = createLabel(labelXPos, barYPos - labelPadding + (barHeight + barPadding) * 4);
-
   var circlesGroup = svg.append('g');
   circlesGroup.selectAll('circle').data(data).enter()
     .append('circle')
@@ -106,18 +47,8 @@ d3.csv("pokemon.csv", function(error, rawData) {
     .attr('r', 6)
     .on('mouseover', function(d) {
       if( d['MATCHED'] ) {
-        pokemonName.text(d['NAME']);
         d3.select(this).attr('style', 'stroke:#555;stroke-width:3px;');
-        cpLabel.text(d['MAX_CP_40'] + ' CP');
-        hpLabel.text(d['MAX_HP_40'] + ' HP');
-        attackLabel.text(d['ATK'] + ' ATK');
-        defenseLabel.text(d['DEF'] + ' DEF');
-        staminaLabel.text(d['STA'] + ' STA');
-        cpBar.transition().attr('width', cpBarScale(d['MAX_CP_40']));
-        hpBar.transition().attr('width', hpBarScale(d['MAX_HP_40']));
-        attackBar.transition().attr('width', attackScale(d['ATK']));
-        defenseBar.transition().attr('width', defenseScale(d['DEF']));
-        staminaBar.transition().attr('width', staminaScale(d['STA']));
+        detailsView.update(d['NAME'], d['MAX_CP_40'], d['MAX_HP_40'], d['ATK'], d['DEF'], d['STA']);
       }
     })
     .on('mouseout', function(d) {
--- src/components/details_view.js
+++ src/components/details_view.js
@@ -1,15 +1,77 @@
 class DetailsView {
   constructor(parent) {
+    this.d3 = require("d3");
     this.root = parent.append('g');
-    this.root.append('rect')
-      .attr('x', 5)
-      .attr('y', 5)
-      .attr('height', 50)
-      .attr('width', 50);
+    this.pokemonName = this.root.append('text')
+      .attr('x', 50)
+      .attr('y', 50)
+      .attr('font-family', 'verdana')
+      .attr('font-size', '16px');
+
+    this.barLength = 100;
+    this.barHeight =  8;
+
+    var barXPos = 50;
+    var barYPos = 80;
+    var barPadding = 20;
+    var labelPadding = 2;
+    var labelXPos = 50;
+
+    this.cpBar = this._createBar(barXPos, barYPos, '#35ADC5');
+    this.hpBar = this._createBar(barXPos, barYPos + (this.barHeight + barPadding), '#B3D5D6');
+    this.attackBar = this._createBar(barXPos, barYPos + (this.barHeight + barPadding) * 2, '#B69689');
+    this.defenseBar = this._createBar(barXPos, barYPos + (this.barHeight + barPadding) * 3, '#D7B19C');
+    this.staminaBar = this._createBar(barXPos, barYPos + (this.barHeight + barPadding) * 4, '#E5D1B9');
+
+    this.cpLabel = this._createLabel(labelXPos, barYPos - labelPadding);
+    this.hpLabel = this._createLabel(labelXPos, barYPos - labelPadding + (this.barHeight + barPadding));
+    this.attackLabel = this._createLabel(labelXPos, barYPos - labelPadding + (this.barHeight + barPadding) * 2);
+    this.defenseLabel = this._createLabel(labelXPos, barYPos - labelPadding + (this.barHeight + barPadding) * 3);
+    this.staminaLabel = this._createLabel(labelXPos, barYPos - labelPadding + (this.barHeight + barPadding) * 4);
+
+    this.cpBarScale = this.d3.scaleLinear().domain([0, 3670]).range([0, this.barLength]);
+    this.hpBarScale = this.d3.scaleLinear().domain([0, 415]).range([0, this.barLength]);
+    this.attackScale = this.d3.scaleLinear().domain([0, 330]).range([0, this.barLength]);
+    this.defenseScale = this.d3.scaleLinear().domain([0, 396]).range([0, this.barLength]);
+    this.staminaScale = this.d3.scaleLinear().domain([0, 510]).range([0, this.barLength]);
   }
 
-  _createLabel() {
+  update(name, cp, hp, atk, def, sta) {
+    this.pokemonName.text(name);
+    this.cpLabel.text(`${cp} CP`);
+    this.hpLabel.text(`${hp} HP`);
+    this.attackLabel.text(`${atk} ATK`);
+    this.defenseLabel.text(`${def} DEF`);
+    this.staminaLabel.text(`${sta} STA`);
+    this.cpBar.transition().attr('width', this.cpBarScale(cp));
+    this.hpBar.transition().attr('width', this.hpBarScale(hp));
+    this.attackBar.transition().attr('width', this.attackScale(atk));
+    this.defenseBar.transition().attr('width', this.defenseScale(def));
+    this.staminaBar.transition().attr('width', this.staminaScale(sta));
+  }
 
+  _createLabel(x, y) {
+    return this.root.append('text')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('font-family', 'verdana')
+      .attr('font-size', '10px');
+  }
+
+  _createBar(x, y, fill) {
+    this.root.append('rect')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('height', this.barHeight)
+      .attr('fill', '#eee')
+      .attr('width', this.barLength);
+
+    return this.root.append('rect')
+      .attr('x', x)
+      .attr('y', y)
+      .attr('height', this.barHeight)
+      .attr('fill', fill)
+      .attr('width', 0);
   }
 }
 
